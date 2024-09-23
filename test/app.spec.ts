e5776a7440056d40c77c4732141371c8e8437c30
Chung Minh Tu <chungminhtu@gmail.com>
2024-09-23 19:18:52 +0800

    add vite and vitest

--- test/app.spec.ts
+++ test/app.spec.ts
@@ -1,6 +1,6 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { INestApplication, ValidationPipe } from '@nestjs/common';
-import * as request from 'supertest';
+import request from 'supertest';
 import { AppModule } from '../src/app.module';
 import { ObjectId } from 'mongodb';
 import { MongoMemoryServerFactory } from '../src/common/mongodb-memory-server.provider';
