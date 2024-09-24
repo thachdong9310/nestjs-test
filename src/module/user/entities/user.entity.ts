af7f9788f27a50f1e077d7db60d7d360b75e3558
thachdong <oscntt6@ViettelGroup.ViettelAD.com>
2024-09-24 08:03:30 +0700

    add type to entities

--- src/module/user/entities/user.entity.ts
+++ src/module/user/entities/user.entity.ts
@@ -1,37 +1,39 @@
 import { ObjectId } from 'mongodb';
-import { COLUMN_NAMES, ENTITY_NAMES } from '../../../common/constants/database.constants';
 import { Column, Entity, ObjectIdColumn } from 'typeorm';
+import { COLUMN_NAMES, ENTITY_NAMES } from '../../../common/constants/database.constants';
 
 
 @Entity({ name: ENTITY_NAMES.USER, orderBy: { firstName: 'ASC' } })
 export class User {
-    @ObjectIdColumn()
-    _id: ObjectId;
+  @ObjectIdColumn()
+  _id: ObjectId;
 
-    // @OneToMany(() => Project, (project) => project.user)
-    // projects: Project[];
+  // @OneToMany(() => Project, (project) => project.user)
+  // projects: Project[];
 
-    // @OneToMany(() => Task, (task) => task.user)
-    // tasks: Task[];
+  // @OneToMany(() => Task, (task) => task.user)
+  // tasks: Task[];
 
-    @Column({
-        name: COLUMN_NAMES.USER.FIRST_NAME,
-        nullable: false,
-        length: 100,
-    })
-    firstName: string;
+  @Column({
+    type: 'string',
+    name: COLUMN_NAMES.USER.FIRST_NAME,
+    nullable: false,
+    length: 100,
+  })
+  firstName: string;
 
-    @Column({ name: COLUMN_NAMES.USER.LAST_NAME, nullable: false, length: 100 })
-    lastName: string;
+  @Column({ type: 'string', name: COLUMN_NAMES.USER.LAST_NAME, nullable: false, length: 100 })
+  lastName: string;
 
-    @Column({
-        name: COLUMN_NAMES.USER.EMAIL,
-        unique: true,
-        nullable: false,
-        length: 255,
-    })
-    email: string;
+  @Column({
+    type: 'string',
+    name: COLUMN_NAMES.USER.EMAIL,
+    unique: true,
+    nullable: false,
+    length: 255,
+  })
+  email: string;
 
-    @Column({ name: COLUMN_NAMES.USER.PASSWORD, nullable: false })
-    password: string;
+  @Column({ type: 'string', name: COLUMN_NAMES.USER.PASSWORD, nullable: false })
+  password: string;
 }
